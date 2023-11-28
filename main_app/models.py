f594eb1cddbbd703499c3e8051b693bb7515a33d
Ahmadwali1 <walia3894@gmail.com>
2023-11-28 20:03:37 +0500

    create guide model in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Guide(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=1000)
+  price = models.IntergerField()
+
+  def __str__(self):
+    return f'{self.name} ({self.id})'
