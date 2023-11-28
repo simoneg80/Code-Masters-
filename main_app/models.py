388e6768dd2bcd2cc135ad091022a060f9e7a05e
Ahmadwali1 <walia3894@gmail.com>
2023-11-28 20:12:12 +0500

    update the guide model

--- main_app/models.py
+++ main_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Guide(models.Model):
   name = models.CharField(max_length=100)
-  description = models.CharField(max_length=1000)
-  price = models.IntergerField()
+  description = models.CharField(max_length=100)
+  price = models.IntegerField()
 
   def __str__(self):
     return f'{self.name} ({self.id})'
