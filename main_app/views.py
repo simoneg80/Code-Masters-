5bb0df263a02f2971a42a321f2f7a8c5b14089bf
Simone Green <simoneg80@yahoo.com>
2023-11-28 10:35:27 -0500

    Update index view and template

--- main_app/views.py
+++ main_app/views.py
@@ -4,24 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-
-
-
-
 # Path for About Page
 def about(request):
     return render(request, 'about.html')
 
-
-
-
-
-
-
-
-
-
-
-# Path for Shop Page
+# Path for Shop All Gudies Page
 def guides_index(request):
-    return render(request, 'guides/index.html')
+    guides = Guide.objects.all()
+    return render(request, 'guides/index.html', {'guides': guides})
