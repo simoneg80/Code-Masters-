d54152836507db27f5099fcf2f0fd71ad7a4ad27
Ahmadwali1 <walia3894@gmail.com>
2023-11-30 01:23:26 +0500

    create sign up function in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -46,3 +46,18 @@ class OrderUpdate(UpdateView):
 class OrderDelete(DeleteView):
     model = Order
     success_url = '/orders/'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+   
