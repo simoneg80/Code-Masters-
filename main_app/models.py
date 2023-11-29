37a86abb3bc0b4285f00ec29c755252fb2cd8d60
Ahmadwali1 <walia3894@gmail.com>
2023-11-29 21:01:02 +0500

    import User and put user in Guide in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Guide(models.Model):
   name = models.CharField(max_length=100)
   description = models.CharField(max_length=100)
   price = models.IntegerField()
+
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return f'{self.name} ({self.id})'
