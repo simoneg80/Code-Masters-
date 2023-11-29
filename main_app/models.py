d37a902de5b65b57c9d084ee1fc5c52def1ba638
Simone Green <simoneg80@yahoo.com>
2023-11-29 11:37:56 -0500

    Set up model for orders

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
+
+# Step1: Create a model M:M Model for order
+class Order(models.Model):
+  date = models.DateField()
+  
+
+  def __str__(self):
+    return f'Order # {self.id} on {self.date}'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'pk': self.id})
+
+
 # Create your models here.
 class Guide(models.Model):
   name = models.CharField(max_length=100)
   description = models.CharField(max_length=100)
   price = models.IntegerField()
+# Step2: Add a ManyToManyField to Order
+  orders = models.ManyToManyField(Order, blank=True)
 
   def __str__(self):
     return f'{self.name} ({self.id})'
