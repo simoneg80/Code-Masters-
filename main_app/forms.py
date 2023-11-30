22914cee8d63343ee5f6afa0b7a15d35fbf8caf3
Ahmadwali1 <walia3894@gmail.com>
2023-12-01 02:09:11 +0500

    import and create commentform in form.py

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
-from .models import Guide, Order
+from .models import Guide, Order, Comment
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = "__all__"
+
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['date', 'comment']
         
