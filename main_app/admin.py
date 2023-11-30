1dc8d219f17a7a3b8280de43ad3cf35113f08190
Simone Green <simoneg80@yahoo.com>
2023-11-30 09:53:56 -0500

    update admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Guide, Order, User
+from .models import Guide, Order
 
 # Register your models here.
 admin.site.register(Guide)
 admin.site.register(Order)
-admin.site.register(User)
+
